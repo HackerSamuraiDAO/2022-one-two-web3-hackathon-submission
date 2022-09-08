98d1aefed03a28c1053c4a9c3eb9d400b8368cf4
Aqrare <64068653+Aqrare@users.noreply.github.com>
2022-09-08 18:13:03 +0900

    Delete template/packages/frontend directory

--- template/packages/frontend/src/components/Footer/data.tsx
+++
@@ -1,5 +0,0 @@
-import { FaGithub } from "react-icons/fa";
-
-export const icons = {
-  github: <FaGithub size="1.25rem" color="gray" />,
-};
--- template/packages/frontend/src/type/confg.ts
+++
@@ -1,5 +0,0 @@
-// export "" from ""
-import configs from "../../config.json";
-
-export type LinkKey = keyof typeof configs.links;
-export type WalletKey = keyof typeof configs.wallets;
